61be10632c43dbe3f16e887c0cd08539d2bd4cd0
Ben-21 <bs@schaefer-inet.de>
2023-07-01 17:50:30 +0200

    add checkIfNumbersAreIncluded()

--- src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Checkings.java
+++ src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Checkings.java
@@ -9,4 +9,8 @@ public class Checkings {
         return password.getWord().length();
 
     }
+
+    public boolean checkIfNumbersAreIncluded(Password password) {
+        return password.getWord().matches(".*\\d.*");
+    }
 }
