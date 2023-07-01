1a93045a1609b46cabd9e63b235647a009da4188
Ben-21 <bs@schaefer-inet.de>
2023-07-01 16:49:32 +0200

    add checkPasswordLength

--- src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Methods.java
+++ src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Methods.java
@@ -11,5 +11,9 @@ import lombok.RequiredArgsConstructor;
 public class Methods {
 
 
+    public int checkPasswordLength(Password password) {
 
+        return password.getWord().length();
+
+    }
 }
