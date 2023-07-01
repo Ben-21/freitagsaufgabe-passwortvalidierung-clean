7e4cba27ba540c59126ae36a250cc254794685e0
Ben-21 <bs@schaefer-inet.de>
2023-07-01 17:00:05 +0200

    removed lombok constructors in Checkings.java

--- src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Checkings.java
+++ src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Checkings.java
@@ -1,13 +1,6 @@
 package de.neuefische.freitagsaufgabepasswortvalidierungclean;
 
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@RequiredArgsConstructor
 public class Checkings {
 
 
