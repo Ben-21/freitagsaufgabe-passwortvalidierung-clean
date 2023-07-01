56bbfe89ad938ef96483b70a57072211b78f7923
Ben-21 <bs@schaefer-inet.de>
2023-07-01 17:57:13 +0200

    add checkIfCapitalLettersAreIncluded()

--- src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Checkings.java
+++ src/main/java/de/neuefische/freitagsaufgabepasswortvalidierungclean/Checkings.java
@@ -9,4 +9,22 @@ public class Checkings {
         return password.getWord().length();
 
     }
+
+    public boolean checkIfCapitalLettersAreIncluded(Password password) {
+
+        String word = password.getWord();
+
+        for (int i = 0; i < word.length(); i++) {
+
+            if (Character.isUpperCase(word.charAt(i))) {
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
 }
